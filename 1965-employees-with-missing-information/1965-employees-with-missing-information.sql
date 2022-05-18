cfd8ced9938c5e490ae03c9a08f1a743b1a8dcc7
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-05-18 23:55:04 +0200

    Time: 607 ms (71.98%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+select employee_id
+from(
+    select employee_id from Employees
+    union all select employee_id from Salaries
+)  t
+group by employee_id
+having count(*)=1
+order by 1
