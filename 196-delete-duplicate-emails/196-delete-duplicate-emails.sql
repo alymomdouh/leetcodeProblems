9c3ee61c22f7e0c32dff27b0c24bb858c6208d00
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-05-21 21:42:10 +0200

    Time: 1733 ms (5.01%), Space: 0B (100.00%) - LeetHub

---
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -0,0 +1,7 @@
+/* 
+ Please write a DELETE statement and DO NOT write a SELECT statement.
+ Write your T-SQL query statement below
+ */
+delete p1 from Person as p1 ,Person as p2
+where p1.Email=p2.Email and p1.Id>p2.Id;
+select * from Person;
