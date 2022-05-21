d8d943e2a688239c4719b2450fcc3320edc45242
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-05-21 21:33:49 +0200

    Time: 576 ms (40.43%), Space: 0B (100.00%) - LeetHub

---
+++ 627-swap-salary/627-swap-salary.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+--update salary set sex = CHAR(ASCII('f') ^ ASCII('m') ^ ASCII(sex));
+-- ather solution 
+UPDATE salary
+    SET sex  = (CASE WHEN sex = 'm' 
+        THEN  'f' 
+        ELSE 'm' 
+        END)
