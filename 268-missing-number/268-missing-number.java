958ec4b5d41b4bb7a795c243743c73d0a95ead83
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-06-03 23:15:31 +0200

    Time: 0 ms (100.00%), Space: 43.2 MB (81.52%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,6 +1,5 @@
-class Solution {
-    
-    public int missingNumber(int[] nums) {
+class Solution { 
+    public int missingNumber1(int[] nums) {
         int n = nums.length + 1;
         int sum = n*(n-1)/2;
 
@@ -9,6 +8,14 @@ class Solution {
         }
 
         return sum;
+    }
+    public int missingNumber(int[] nums) {
+        int missing = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            missing ^= i^nums[i];
+        }
+
+        return missing;
     }
 
 }
