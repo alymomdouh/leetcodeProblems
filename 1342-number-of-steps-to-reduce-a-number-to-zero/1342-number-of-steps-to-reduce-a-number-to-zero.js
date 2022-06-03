e2234af37e77a6c0511e0caaad80122927bf945f
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-06-03 23:21:28 +0200

    Time: 114 ms (8.19%), Space: 39.1 MB (100.00%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps  = function(num) {
+    let ans = 0
+    for (; num; ans++)
+        if (num % 2) num--
+        else num /= 2
+    return ans
+};
