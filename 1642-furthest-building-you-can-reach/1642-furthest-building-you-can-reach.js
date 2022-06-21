8e599560a1bc87ebb3365d9747955fd25475d092
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-06-21 20:17:51 +0200

    Time: 247 ms (32.31%), Space: 57.8 MB (33.85%) - LeetHub

---
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} heights  =>h heights 
+ * @param {number} bricks  => b  bricks
+ * @param {number} ladders =>l  ladder 
+ * @return {number}
+ */
+//w/ MinPriorityQueue():
+var furthestBuilding = function(H, B, L) {
+    let len = H.length - 1,
+        pq = new MinPriorityQueue({priority: x => x})
+    for (let i = 0; i < len; i++) {
+        let diff = H[i+1] - H[i]
+        if (diff > 0) {
+            if (L > 0) pq.enqueue(diff), L--
+            else if (pq.front() && diff > pq.front().element)
+                pq.enqueue(diff), B -= pq.dequeue().element
+            else B -= diff
+            if (B < 0) return i
+        }
+    }
+    return len
+};
