87468edfbc01f6569ee1dd8acb564fd13dc4077f
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-06-22 22:28:02 +0200

    Time: 7 ms (47.90%), Space: 45.6 MB (11.20%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -6,28 +6,60 @@
 // }
 /// other solation 
 //Min Heap of size N
+// class Solution {
+
+//     public int findKthLargest(int[] nums, int k) {
+//         int n = nums.length;
+
+//         if (n == 1) {
+//             return nums[0];
+//         }
+
+//         PriorityQueue<Integer> minHeap = new PriorityQueue();
+
+//         for(int num: nums){
+//             minHeap.offer(num);
+//         }
+
+//         int i = 0;
+//         int kThLargest = minHeap.poll();
+
+//         while (i++ < (n - k)) {
+//             kThLargest = minHeap.poll();
+//         }
+
+//         return kThLargest;
+//     }
+// }
+
+//Max Heap of size N
 class Solution {
-
     public int findKthLargest(int[] nums, int k) {
-        int n = nums.length;
 
-        if (n == 1) {
+        int len = nums.length;
+
+        if(len == 1){
             return nums[0];
         }
 
-        PriorityQueue<Integer> minHeap = new PriorityQueue();
+// Since we are using Max-Heap, we need to sortaccordingly
+    Comparator<Integer> comp = (a,b) -> b.compareTo(a);
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<>(comp);
 
+        // Add all the elements
         for(int num: nums){
-            minHeap.offer(num);
+            maxHeap.offer(num);
         }
 
-        int i = 0;
-        int kThLargest = minHeap.poll();
+        // we need to poll for k-1 times and
+        // return the next polled element
+        int i = 1;
 
-        while (i++ < (n - k)) {
-            kThLargest = minHeap.poll();
+        while(i++ < k){
+            maxHeap.poll();
         }
 
-        return kThLargest;
+        return  maxHeap.poll();
+
     }
 }
