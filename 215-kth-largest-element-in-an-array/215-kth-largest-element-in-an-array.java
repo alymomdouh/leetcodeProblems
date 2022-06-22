1499879eaf82bea77760dd9eb78a82b2d86098b8
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-06-22 22:26:10 +0200

    Time: 12 ms (23.79%), Space: 45.8 MB (6.54%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -1,6 +1,33 @@
+// class Solution {
+//     public int findKthLargest(int[] nums, int k) {
+//         Arrays.sort(nums);
+//         return nums[nums.length-k];
+//     }
+// }
+/// other solation 
+//Min Heap of size N
 class Solution {
+
     public int findKthLargest(int[] nums, int k) {
-        Arrays.sort(nums);
-        return nums[nums.length-k];
+        int n = nums.length;
+
+        if (n == 1) {
+            return nums[0];
+        }
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue();
+
+        for(int num: nums){
+            minHeap.offer(num);
+        }
+
+        int i = 0;
+        int kThLargest = minHeap.poll();
+
+        while (i++ < (n - k)) {
+            kThLargest = minHeap.poll();
+        }
+
+        return kThLargest;
     }
 }
