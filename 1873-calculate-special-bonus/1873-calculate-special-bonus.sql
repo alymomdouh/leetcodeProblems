45fc5a16e17c7da77ec0d219f35e1fcce250e22b
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-08-04 23:01:34 +0200

    Time: 1355 ms (42.62%), Space: 0B (100.00%) - LeetHub

--- 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -1,14 +1,7 @@
-/* Write your T-SQL query statement below */
--- # Write your MySQL query statement below
-
-
-
- 
-
-SELECT employee_id  , CAST(
-             CASE
-                  WHEN employee_id % 2 = 0 or name like 'M%'
-                     THEN 0
-                  ELSE salary
-             END as int ) as "Bonus"
-FROM Employees
+--- Write your MySQL query statement below
+SELECT employee_id,                                                   
+CASE WHEN employee_id%2 <> 0 AND name not like 'M%' THEN salary    
+ELSE 0  
+END as bonus  
+FROM Employees  
+ORDER BY employee_id;
