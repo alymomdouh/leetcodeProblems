1a7824c8c205bd40927c9fc7f86fedbc9f6e63d9
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-08-05 18:30:29 +0200

    Time: 97 ms (54.03%), Space: 42.2 MB (94.38%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
@@ -6,7 +6,7 @@
 var intersect = function(nums1, nums2) {
     let res = [];
     for (var i = 0; i < nums2.length; i++){
-        if (nums1.includes(nums2[i])){
+        if (nums1.includes(nums2[i]) ){
             res.push(nums2[i]);
             nums1.splice(nums1.indexOf(nums2[i]), 1);
         }
