2365076c4b39fec56b254d2ea240c15988a301a8
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-08-05 17:43:04 +0200

    Time: 92 ms (56.87%), Space: 42.2 MB (80.96%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.js
+++ 377-combination-sum-iv/377-combination-sum-iv.js
@@ -4,11 +4,22 @@
  * @return {number}
  */
 //w/ Top-Down DP:
+// var combinationSum4 = function(N, T) {
+//     let dp = new Uint32Array(T+1)
+//     dp[0] = 1
+//     for (let i = 1; i <= T; i++)
+//         for (let num of N)
+//             if (num <= i) dp[i] += dp[i-num]
+//     return dp[T]
+// };
+//w/ Bottom-Up DP:
 var combinationSum4 = function(N, T) {
     let dp = new Uint32Array(T+1)
     dp[0] = 1
-    for (let i = 1; i <= T; i++)
+    for (let i = 0; i < T; i++) {
+        if (!dp[i]) continue
         for (let num of N)
-            if (num <= i) dp[i] += dp[i-num]
+            if (num + i <= T) dp[i+num] += dp[i]
+    }
     return dp[T]
 };
