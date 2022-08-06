6ee030cd7ffd2c5ac6c24d47a3298bd9ff7545b2
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-08-06 18:55:36 +0200

    Time: 263 ms (15.63%), Space: 93.3 MB (89.63%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,103 @@
+//Runtime: 8 ms, faster than 99.32% of C++ online submissions for Best Time to Buy and Sell Stock.
+//Memory Usage: 9.4 MB, less than 94.64% of C++ online submissions for Best Time to Buy and Sell Stock.
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() == 0) return 0;
+        
+        int ans = 0;
+        
+        int start = prices[0], end = prices[0];
+        
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < start){
+                //restart as session
+                ans = max(ans, end - start);
+                start = prices[i];
+                end = prices[i];
+            }else{
+                //continue current session
+                end = max(end, prices[i]);
+            }
+        }
+        ans = max(ans, end - start);
+        return ans;
+    }
+};
+
+/**
+Approach 2: One Pass
+Algorithm
+Say the given array is:
+[7, 1, 5, 3, 6, 4]
+If we plot the numbers of the given array on a graph, we get:
+The points of interest are the peaks and valleys in the given graph. We need to find the largest peak following the smallest valley. We can maintain two variables - minprice and maxprofit corresponding to the smallest valley and maximum profit (maximum difference between selling price and minprice) obtained so far respectively.
+**/
+
+/**
+Time complexity : O(n). Only a single pass is needed.
+Space complexity : O(1). Only two variables are used.
+**/
+
+
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int valley = INT_MAX;
+//         int ans = 0;
+//         for(int i = 0; i < prices.size(); i++){
+//             if(prices[i] < valley){
+//                 valley = prices[i];
+//             }else if(prices[i] - valley > ans){
+//                 ans = prices[i] - valley;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+//Greedy
+//Runtime: 4 ms, faster than 97.14% of C++ online submissions for Best Time to Buy and Sell Stock.
+//Memory Usage: 7.5 MB, less than 100.00% of C++ online submissions for Best Time to Buy and Sell Stock.
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+        
+//         int maxToTail = 0;
+//         int profit, maxProfit = 0;
+        
+//         for(int i = n-1; i >= 0; i--){
+//             maxToTail = max(maxToTail, prices[i]);
+//             profit = max(maxToTail - prices[i], 0);
+//             maxProfit = max(maxProfit, profit);
+//         }
+        
+//         return maxProfit;
+//     }
+// };
+
+//Greedy
+//Runtime: 8 ms, faster than 49.52% of C++ online submissions for Best Time to Buy and Sell Stock.
+//Memory Usage: 13 MB, less than 5.51% of C++ online submissions for Best Time to Buy and Sell Stock.
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int ans = 0;
+//         int buy = INT_MAX;
+//         int cash = 0;
+        
+//         for(int price : prices){
+//             /*
+//             meaningless for first iteration,
+//             in which buy is not set
+//             */
+//             cash = max(cash, price-buy);
+//             buy = min(buy, price);
+//             ans = max(ans, cash);
+//         }
+        
+//         return ans;
+//     }
+// };
