6419f908ea565e69cc2b55faf327d1504b0f24b1
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-08-09 07:28:16 +0200

    Time: 1648 ms (6.88%), Space: 0B (100.00%) - LeetHub

--- 197-rising-temperature/197-rising-temperature.sql
+++ 197-rising-temperature/197-rising-temperature.sql
@@ -1,3 +1,8 @@
 /* Write your T-SQL query statement below */
-SELECT weather.id AS 'Id' FROM  weather JOIN weather w 
-ON DATEDIFF(day,w.recordDate, weather.recordDate) = 1 AND weather.Temperature > w.Temperature;
+-- SELECT weather.id AS 'Id' FROM  weather JOIN weather w 
+-- ON DATEDIFF(day,w.recordDate, weather.recordDate) = 1 AND weather.Temperature > w.Temperature;
+---
+SELECT DISTINCT a.Id
+FROM Weather a,Weather b
+WHERE a.Temperature > b.Temperature
+AND DATEDIFF(day,b.Recorddate, a.Recorddate) = 1
