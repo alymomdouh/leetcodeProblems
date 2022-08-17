ecb23c2bc34b69c09b4ba0cf7e6e9b10d89a6f65
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-08-17 21:08:20 +0200

    Time: 2 ms (99.55%), Space: 40.1 MB (99.04%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.java
+++ 804-unique-morse-code-words/804-unique-morse-code-words.java
@@ -26,18 +26,3 @@ class Solution {
     }
 }
 //Your runtime beats 85.31 % of cpp submissions.
-// class Solution {
-// public:
-//     int uniqueMorseRepresentations(vector<string>& words) {
-//         string morse[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
-//         set<string> uniqueMorses;
-//         for(int i = 0; i < words.size(); i++){
-//             string s;
-//             for(int j = 0; j < words[i].size(); j++){
-//                 s+=morse[(int)(words[i][j]-'a')];
-//             }
-//             uniqueMorses.insert(s);
-//         }
-//         return uniqueMorses.size();
-//     }
-// };
