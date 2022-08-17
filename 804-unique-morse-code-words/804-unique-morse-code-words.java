5a2c8296083cf21d154818bf17ae2e686ed2c3c9
aly momdouh aly <33808080+alymomdouh@users.noreply.github.com>
2022-08-17 21:07:42 +0200

    Time: 2 ms (99.55%), Space: 42.2 MB (44.14%) - LeetHub

---
+++ 804-unique-morse-code-words/804-unique-morse-code-words.java
@@ -0,0 +1,43 @@
+/**
+Approach #1: Hash Set [Accepted]
+Intuition and Algorithm
+We can transform each word into it's Morse Code representation.
+After, we put all transformations into a set seen, and return the size of the set.
+Java
+
+Complexity Analysis
+Time Complexity: O(S), where S is the sum of the lengths of words in words. We iterate through each character of each word in words.
+Space Complexity: O(S).
+**/
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] MORSE = new String[]{".-","-...","-.-.","-..",".","..-.","--.",
+                         "....","..",".---","-.-",".-..","--","-.",
+                         "---",".--.","--.-",".-.","...","-","..-",
+                         "...-",".--","-..-","-.--","--.."};
+        Set<String> seen = new HashSet();
+        for (String word: words) {
+            StringBuilder code = new StringBuilder();
+            for (char c: word.toCharArray())
+                code.append(MORSE[c - 'a']);
+            seen.add(code.toString());
+        }
+        return seen.size();
+    }
+}
+//Your runtime beats 85.31 % of cpp submissions.
+// class Solution {
+// public:
+//     int uniqueMorseRepresentations(vector<string>& words) {
+//         string morse[] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+//         set<string> uniqueMorses;
+//         for(int i = 0; i < words.size(); i++){
+//             string s;
+//             for(int j = 0; j < words[i].size(); j++){
+//                 s+=morse[(int)(words[i][j]-'a')];
+//             }
+//             uniqueMorses.insert(s);
+//         }
+//         return uniqueMorses.size();
+//     }
+// };
