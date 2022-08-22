a77d532f75d4b0e0fdcf1fde5bb372090d67ff4c
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-08-22 19:54:36 +0200

    Time: 2 ms (65.34%), Space: 41.2 MB (58.81%) - LeetHub

--- 342-power-of-four/342-power-of-four.java
+++ 342-power-of-four/342-power-of-four.java
@@ -1,10 +1,21 @@
-public class Solution {
-    private static final double epsilon = 10e-15;
+  public class Solution {
+//     private static final double epsilon = 10e-15;
  
-    public boolean isPowerOfFour(int num) {
-        if (num == 0)
+//     public boolean isPowerOfFour(int num) {
+//         if (num == 0)
+//             return false;
+//         double res = Math.log(num) / Math.log(4);
+//         return Math.abs(res - Math.round(res)) < epsilon;
+//     }
+ 
+// other soluation
+public boolean isPowerOfFour(int n) {
+        if (n == 0)
             return false;
-        double res = Math.log(num) / Math.log(4);
-        return Math.abs(res - Math.round(res)) < epsilon;
+        if (n == 1)
+            return true;
+        if (n % 4 == 0)
+            return isPowerOfFour(n / 4);
+        return false;
     }
-}
+  }
