d1a53e5e1badc56588bb12b41bb6db20e872f86e
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-09-01 20:13:00 +0200

    Time: 4 ms (40.75%), Space: 59.8 MB (42.89%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.java
@@ -0,0 +1,56 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+/*
+ * @lc app=leetcode id=1448 lang=java
+ *
+ * [1448] Count Good Nodes in Binary Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int goodNodes(TreeNode root) {
+        return helper(root, root.val);
+        
+    }
+
+    public int helper(TreeNode node, int curMax){
+        if(node == null) return 0;
+        int cur = 0;
+        if(node.val>=curMax){
+            cur++;
+            curMax = Math.max(curMax, node.val);
+        }
+        cur += helper(node.left, curMax);
+        cur += helper(node.right, curMax);
+        return cur;
+    }
+}
+// @lc code=end
