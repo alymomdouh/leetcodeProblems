1f7ba72f11d58958e1412c19318bdd371f7b98ba
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-09-06 00:16:10 +0200

    Time: 2 ms (95.53%), Space: 47.2 MB (61.90%) - LeetHub

---
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.java
@@ -0,0 +1,60 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+// Tree + DFS + Recursion solution
+// Time: O(n), 1ms
+// Space: O(h), 40.7mb
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        DFS(root, ans, 1);
+        
+        return ans;
+    }
+    
+    private void DFS(Node root, List<List<Integer>> ans, int depth) {
+        // Base case
+        if(root == null) return;
+        
+        // Add the row when we first reach the level
+        if(ans.size() < depth) {
+            ans.add(new ArrayList<>());
+        }
+        
+        ans.get(depth - 1).add(root.val);
+        
+        // Traverse children
+        for(Node child : root.children) {
+            DFS(child, ans, depth + 1);
+        }
+    }
+}
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+    public Node() {}
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
