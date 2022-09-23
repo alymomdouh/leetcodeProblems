1e820ef30fb126f71c5f2724a9a43fe0fee9da7f
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-09-23 09:45:13 +0200

    Time: 258 ms (55.56%), Space: 43.3 MB (55.56%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.js
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.js
@@ -2,10 +2,18 @@
  * @param {number} n
  * @return {number}
  */
+// var concatenatedBinary = function(n) {
+//     let ans = 1, len = 0b100
+//     for (let i = 2; i <= n; i++) {
+//         if (i === len) len <<= 1
+//         ans = (ans * len + i) % 1000000007
+//     }
+//     return ans
+// };
 var concatenatedBinary = function(n) {
-    let ans = 1, len = 0b100
+    let ans = 1, len = 4
     for (let i = 2; i <= n; i++) {
-        if (i === len) len <<= 1
+        if (i === len) len *= 2
         ans = (ans * len + i) % 1000000007
     }
     return ans
