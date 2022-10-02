bbe4471a60b5f3391bee640544c20c7eefac36a5
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-10-02 20:54:32 +0200

    Time: 102 ms (14.59%), Space: 44.1 MB (22.20%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -0,0 +1,24 @@
+//Dynamic Bottom up
+
+class Solution {
+    public int numRollsToTarget(int d, int f, int target) {
+        int MOD = (int)Math.pow(10, 9) + 7;
+        long[][] dp = new long[d + 1][target + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= d; i++) {
+            for (int j = 0; j <= target; j++) {
+                for (int k = 1; k <= f; k++) {
+                    if (j >= k) {    // dp[i][0] == 0
+                        dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % MOD;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return (int)dp[d][target];
+    }
+}
+/* https://wentao-shao.gitbook.io/leetcode/dynamic-programming/backpack-problem/1155.number-of-dice-rolls-with-target-sum#approach-2-dynamic-bottom-up
+**/
