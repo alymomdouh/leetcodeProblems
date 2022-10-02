9af200d20df98a79ff4a65743ac8d9fc4b4899db
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-10-02 22:28:01 +0200

    Time: 16 ms (92.06%), Space: 43 MB (39.18%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.java
@@ -1,13 +1,13 @@
 //Dynamic Bottom up
-
+/*
 class Solution {
     public int numRollsToTarget(int d, int f, int target) {
         int MOD = (int)Math.pow(10, 9) + 7;
         long[][] dp = new long[d + 1][target + 1];
         dp[0][0] = 1;
-        for (int i = 1; i <= d; i++) {
-            for (int j = 0; j <= target; j++) {
-                for (int k = 1; k <= f; k++) {
+        for (int i = 1; i <= d; i++) {// for dice 
+            for (int j = 0; j <= target; j++) { // for target
+                for (int k = 1; k <= f; k++) { //for faces
                     if (j >= k) {    // dp[i][0] == 0
                         dp[i][j] = (dp[i][j] + dp[i - 1][j - k]) % MOD;
                     } else {
@@ -20,5 +20,24 @@ class Solution {
         return (int)dp[d][target];
     }
 }
-/* https://wentao-shao.gitbook.io/leetcode/dynamic-programming/backpack-problem/1155.number-of-dice-rolls-with-target-sum#approach-2-dynamic-bottom-up
 **/
+/* https://wentao-shao.gitbook.io/leetcode/dynamic-programming/backpack-problem/1155.number-of-dice-rolls-with-target-sum#approach-2-dynamic-bottom-up  **/
+
+class Solution {
+    public int numRollsToTarget(int d, int f, int target) {
+        int[][] dp = new int[d + 1][target + 1];
+        dp[0][0] = 1;
+        int mod=1_000_000_007;
+        for (int i = 1; i <= d; i++) {
+            for (int j = 1; j <= f; j++) {
+                for (int k = target; k >= j; k--) {
+                    if(dp[i-1][k-j]!=0){
+                        dp[i][k]+=dp[i-1][k-j];
+                        dp[i][k]=dp[i][k]% mod;
+                    }
+                }
+            }
+        }
+        return dp[d][target];
+    }
+}
