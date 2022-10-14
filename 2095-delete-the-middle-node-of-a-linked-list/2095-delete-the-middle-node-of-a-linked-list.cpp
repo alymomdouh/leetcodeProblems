7e1aafc0206a7ab2f70e4b2866ae76a72c569c60
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-10-14 11:34:49 +0200

    Time: 2315 ms (6.84%), Space: 294.8 MB (64.88%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -8,20 +8,53 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+//  public:
+//   ListNode* deleteMiddle(ListNode* head) {
+//     ListNode dummy(0, head);
+//     ListNode* slow = &dummy;
+//     ListNode* fast = &dummy;
+
+//     while (fast->next && fast->next->next) {
+//       slow = slow->next;
+//       fast = fast->next->next;
+//     }
+
+//     // Delete the middle node
+//     slow->next = slow->next->next;
+//     return dummy.next;
+//   }
+// };
+
+// 2095.✅ Delete the Middle Node of a Linked List
+
 class Solution {
- public:
-  ListNode* deleteMiddle(ListNode* head) {
-    ListNode dummy(0, head);
-    ListNode* slow = &dummy;
-    ListNode* fast = &dummy;
-
-    while (fast->next && fast->next->next) {
-      slow = slow->next;
-      fast = fast->next->next;
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if(head == NULL)
+            return NULL;
+        
+        if(head -> next == NULL)
+        {
+            delete head;
+            return NULL;
+        }
+        
+        ListNode *slow = head, *fast = head, *prev  = head;
+        
+        while(fast != NULL && fast->next != NULL)
+        {
+            if(slow != head)
+                prev = prev -> next;                    //prev to one step behind of slow
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        
+        //updating prev -> next to slow as slow is middle node
+        prev->next = slow->next;
+        delete slow;
+        
+        return head;
+        
     }
-
-    // Delete the middle node
-    slow->next = slow->next->next;
-    return dummy.next;
-  }
 };
