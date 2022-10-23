fc03b575d21848fa9d209f2ca252354c9c1dd258
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-10-23 21:46:47 +0200

    Time: 27 ms (17.42%), Space: 54.3 MB (68.98%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,17 +1,31 @@
 class Solution {
    //O(N)time O(N)space
+// public int[] findErrorNums(int[] nums) {
+//     int[] t = new int[nums.length];
+//     int res[] = new int[2];
+//     int sum = 0;
+//     for(int i = 0; i < nums.length; i++){
+//         if(t[nums[i] - 1] == 1){
+//             res[0] = nums[i];
+//         }
+//         t[nums[i] - 1] = 1;
+//         sum += i + 1 - nums[i];
+//     }
+//     res[1] = res[0] + sum;
+//     return res;
+// }
+    //O(N)time O(1)space
 public int[] findErrorNums(int[] nums) {
-    int[] t = new int[nums.length];
-    int res[] = new int[2];
+    int dup = -1;
     int sum = 0;
-    for(int i = 0; i < nums.length; i++){
-        if(t[nums[i] - 1] == 1){
-            res[0] = nums[i];
-        }
-        t[nums[i] - 1] = 1;
-        sum += i + 1 - nums[i];
+    for (int i = 0; i < nums.length; i++) {
+        if (nums[Math.abs(nums[i]) - 1] < 0)
+            dup = Math.abs(nums[i]);
+        else
+            nums[Math.abs(nums[i]) - 1] *= -1;
+
+        sum += i + 1 - Math.abs(nums[i]);
     }
-    res[1] = res[0] + sum;
-    return res;
+    return new int[]{dup, dup + sum};
 }
 }
