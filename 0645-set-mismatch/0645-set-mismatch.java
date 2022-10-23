f0670c3d7031aa3f6424e57d7711dca4cbae6e87
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-10-23 21:46:02 +0200

    Time: 9 ms (44.32%), Space: 53.9 MB (80.78%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,17 @@
+class Solution {
+   //O(N)time O(N)space
+public int[] findErrorNums(int[] nums) {
+    int[] t = new int[nums.length];
+    int res[] = new int[2];
+    int sum = 0;
+    for(int i = 0; i < nums.length; i++){
+        if(t[nums[i] - 1] == 1){
+            res[0] = nums[i];
+        }
+        t[nums[i] - 1] = 1;
+        sum += i + 1 - nums[i];
+    }
+    res[1] = res[0] + sum;
+    return res;
+}
+}
