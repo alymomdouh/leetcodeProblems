abac89a4faa4efffcf736342680ea4fef77a171b
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-11-22 13:01:57 +0200

    Time: 3913 ms (5.02%), Space: 0B (100.00%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,5 +1,16 @@
-# Write your MySQL query statement below  in mysql 
-select user_id, 
-    concat(upper(left(name,1)),lower(substring(name,2))) as name
-from Users 
+/* Write your T-SQL query statement below */
+-- select user_id, 
+--     concat(upper(left(name,1)),lower(substring(name,2))) as name
+-- from Users 
+-- order by user_id
+
+-- select user_id,
+--        concat(upper(left(name, 1)), lower(right(name, length(name) - 1))) as name
+-- from Users
+-- group by 1
+-- order by 1;
+
+ select user_id,upper(substring(name,1,1)) 
+    + lower(substring(name,2,9999999)) as name 
+from Users
 order by user_id
