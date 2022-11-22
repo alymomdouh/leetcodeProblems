47c44af38fea4467b0215020b09c131548693c8e
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-11-22 11:25:37 +0200

    Time: 555 ms (53.44%), Space: 0B (100.00%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,9 +1,9 @@
-# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
+--Write your MySQL query statement below
 SELECT * FROM Patients
 WHERE conditions like '% DIAB1%' OR conditions like'DIAB1%'
 
 
 
 
-# SELECT * FROM Patients
-# WHERE regexp_like(conditions, ' +DIAB1|^DIAB1')
+-- SELECT * FROM Patients WHERE regexp_like(conditions, ' +DIAB1|^DIAB1')
