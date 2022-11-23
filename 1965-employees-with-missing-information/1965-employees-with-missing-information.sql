eedb471e3c5f55a8d96f02880a57b3d89b9aff78
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-11-23 10:09:28 +0200

    Time: 999 ms (23.21%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,7 +1,7 @@
 /* Write your T-SQL query statement below */
 select employee_id
 from(
-    select employee_id from Employees
+    select employee_id from Employees  
     union all select employee_id from Salaries
 )  t
 group by employee_id
