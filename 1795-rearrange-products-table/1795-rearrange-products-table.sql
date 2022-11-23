88324ef6db063323f3c55586aabcd546492d1801
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-11-23 10:17:20 +0200

    Time: 635 ms (96.48%), Space: 0B (100.00%) - LeetHub

--- 1795-rearrange-products-table/1795-rearrange-products-table.sql
+++ 1795-rearrange-products-table/1795-rearrange-products-table.sql
@@ -1,4 +1,5 @@
 /* Write your T-SQL query statement below */
+/*
 select product_id,'store1' as store, store1 as price 
 from products 
 where store1 is not null
@@ -14,3 +15,22 @@ union
 select product_id,'store3' as store, store3 as price 
 from products 
 where store3 is not null
+*/
+/*
+select * from 
+(
+    select product_id, 'store1' store, store1 price from Products
+    union 
+    select product_id, 'store2' store, store2 price from Products
+    union
+    select product_id, 'store3' store, store3 price from Products
+)t 
+where price is not null;
+*/
+SELECT product_id,store,price
+FROM Products
+UNPIVOT
+(
+	price
+	FOR store in (store1,store2,store3)
+) AS StorePivot;
