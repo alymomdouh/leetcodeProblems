4eae94c363f524e6ad7d54b6dbbcda9f2cf7e07d
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-11-24 20:54:13 +0200

    Time: 2037 ms (50.68%), Space: 0B (100.00%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,3 +1,2 @@
 /* Write your T-SQL query statement below */
-SELECT DISTINCT author_id as id FROM Views
-Where author_id = viewer_id ORDER BY author_id ASC;
+SELECT DISTINCT author_id as id FROM Views Where author_id = viewer_id ORDER BY author_id ASC;
