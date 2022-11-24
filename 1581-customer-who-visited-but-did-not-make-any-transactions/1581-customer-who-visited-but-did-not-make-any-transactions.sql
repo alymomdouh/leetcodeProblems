322057724258467817da166444e09a46a7a5383d
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-11-24 20:48:36 +0200

    Time: 2818 ms (53.92%), Space: 0B (100.00%) - LeetHub

--- 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ 1581-customer-who-visited-but-did-not-make-any-transactions/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,3 +1,2 @@
 /* Write your T-SQL query statement below */
-
 select v.customer_id,COUNT(*) as count_no_trans  from Visits v where v.visit_id not in(select visit_id  from Transactions) GROUP BY customer_id;
