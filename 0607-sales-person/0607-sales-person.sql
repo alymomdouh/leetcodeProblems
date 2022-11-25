eb3a3ce49497a890917f0d51077c07868d00b038
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-11-25 10:28:10 +0200

    Time: 1157 ms (70.00%), Space: 0B (100.00%) - LeetHub

--- 0607-sales-person/0607-sales-person.sql
+++ 0607-sales-person/0607-sales-person.sql
@@ -1,6 +1,12 @@
 /* Write your T-SQL query statement below */
-SELECT salesperson.name FROM salesperson WHERE 
-salesperson.sales_id not in (
- SELECT orders.sales_id FROM orders
- Left JOIN company on orders.com_id = company.com_id
-        WHERE company.name = 'RED')
+-- SELECT salesperson.name FROM salesperson WHERE 
+-- salesperson.sales_id not in (
+--  SELECT orders.sales_id FROM orders
+--  Left JOIN company on orders.com_id = company.com_id
+--         WHERE company.name = 'RED')
+        
+ --- other solution 
+ select salesperson.name
+from orders o join company c on (o.com_id = c.com_id and c.name = 'RED')
+right join salesperson on salesperson.sales_id = o.sales_id
+where o.sales_id is null;
