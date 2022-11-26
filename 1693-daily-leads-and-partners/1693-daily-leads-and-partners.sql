d120c52fcdb00985e24bb53d4856cfb21af333a2
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-11-26 15:28:47 +0200

    Time: 988 ms (38.08%), Space: 0B (100.00%) - LeetHub

---
+++ 1693-daily-leads-and-partners/1693-daily-leads-and-partners.sql
@@ -0,0 +1,11 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    date_id,
+    make_name,
+    count(distinct lead_id) unique_leads,
+    count(distinct partner_id) unique_partners
+    from DailySales
+    group by date_id,make_name 
+ 
+ 
+ 
