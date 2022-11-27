aa7fd1bc885e1df6343dc5a38dfc2cc22eb1d143
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-11-27 09:46:41 +0200

    Time: 899 ms (81.78%), Space: 0B (100.00%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,7 +1,9 @@
 /* Write your T-SQL query statement below */
-select customer_number from orders group by customer_number 
-    having count(order_number)=(select max(c) from (select count(order_number) c from orders group by customer_number) t );
+--select customer_number from orders group by customer_number 
+--    having count(order_number)=(select max(c) from (select count(order_number) c from orders group by customer_number) t );
 
 
-
+select top 1 customer_number from
+ (select customer_number, count(order_number) order_count  from orders group by customer_number) a 
+order by order_count desc  
 
