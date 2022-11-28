54a9fe29138f7bdb3dc8887dd971e31f42c94978
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-11-28 12:42:10 +0200

    Time: 4170 ms (5.16%), Space: 0B (100.00%) - LeetHub

--- 1407-top-travellers/1407-top-travellers.sql
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -1,4 +1,6 @@
 /* Write your T-SQL query statement below */
-SELECT name, COALESCE(SUM(distance), 0) AS travelled_distance
-FROM users u LEFT JOIN rides r ON u.id = r.user_id
-GROUP BY u.name,u.id ORDER BY travelled_distance DESC, u.name
+SELECT name, COALESCE(SUM(distance), 0) AS travelled_distance FROM users u LEFT JOIN rides r ON u.id = r.user_id
+GROUP BY u.name,u.id ORDER BY travelled_distance DESC, u.name;
+
+
+ 
