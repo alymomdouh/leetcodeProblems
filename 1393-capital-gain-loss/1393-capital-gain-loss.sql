408fb9fe42450c72ae10b48fe9b1033aa6032076
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-11-28 12:22:36 +0200

    Time: 1402 ms (98.50%), Space: 0B (100.00%) - LeetHub

--- 1393-capital-gain-loss/1393-capital-gain-loss.sql
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -1,2 +1,9 @@
 /* Write your T-SQL query statement below */
-SELECT stock_name, SUM(CASE WHEN operation = 'Buy' THEN (-1*price) ELSE price END ) AS capital_gain_loss FROM Stocks GROUP BY stock_name
+-- SELECT stock_name, SUM(CASE WHEN operation = 'Buy' THEN (-1*price) ELSE price END ) AS capital_gain_loss FROM Stocks GROUP BY stock_name ;
+
+SELECT
+    src.stock_name AS stock_name,
+    SUM(src.price) AS capital_gain_loss
+FROM
+(SELECT s.stock_name AS stock_name,CASE WHEN s.operation = 'Buy' THEN -s.price ELSE s.price END AS price
+    FROM Stocks AS s ) AS src GROUP BY src.stock_name;
