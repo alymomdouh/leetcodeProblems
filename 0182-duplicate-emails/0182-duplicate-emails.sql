ea34884bdc7d9b62ad8ca221699d26d136846abd
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-11-29 09:40:38 +0200

    Time: 917 ms (40.37%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,3 +1,7 @@
 /* Write your T-SQL query statement below */
 
-select Email from (select Email, count(Email) as num from Person group by Email)as x where num > 1 ;
+--select Email from (select Email, count(Email) as num from Person group by Email)as x where num > 1 ;
+
+
+
+select Email from Person group by Email having count(Email) > 1;
