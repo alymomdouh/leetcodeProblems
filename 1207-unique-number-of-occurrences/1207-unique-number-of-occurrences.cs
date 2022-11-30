71219e6e42b06f1d48eb36e9284e5c6aaec0bce5
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-11-30 14:15:15 +0200

    Time: 163 ms (35.54%), Space: 39.3 MB (26.07%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cs
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cs
@@ -1,3 +1,36 @@
+//Approach 1: Counting Sort
+
+public class Solution {
+    public bool UniqueOccurrences(int[] arr) {
+        int K = 1000; 
+         int[] freq = new int[2 * K + 1]; 
+        // Store the frequency of elements in the unordered map.
+        foreach  (int num in arr) {
+            freq[num + K]++;
+        } 
+        // Sort the frequency count.
+        Array.Sort(freq); 
+        // If the adjacent freq count is equal, then the freq count isn't unique.
+        for (int i = 0; i < 2 * K; i++) {
+            if (freq[i] != 0 && freq[i] == freq[i + 1]) {
+                return false;
+            }
+        } 
+        // If all the elements are traversed, it implies frequency counts are unique.
+        return true;
+        
+        // var occ = arr.GroupBy(x => x, (int key, IEnumerable<int> all) => all.Count());
+        // var set = new HashSet<int>();
+        // foreach (var o in occ) {
+        //     if (!set.Add(o)) {
+        //         return false;
+        //     }
+        // }
+        // return true;
+    }
+}
+
+/*
 public class Solution {
     public bool UniqueOccurrences(int[] arr) {
         var occ = arr.GroupBy(x => x, (int key, IEnumerable<int> all) => all.Count());
@@ -10,3 +43,4 @@ public class Solution {
         return true;
     }
 }
+*/
