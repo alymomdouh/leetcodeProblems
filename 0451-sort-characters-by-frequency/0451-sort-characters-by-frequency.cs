a3d664f649ec9b9083444a66b49d07b966df1939
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-12-03 09:26:05 +0200

    Time: 209 ms (15.13%), Space: 40.2 MB (44.08%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cs
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cs
@@ -8,11 +8,33 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class Solution {
+    /*
     public string FrequencySort(string s) {
         IDictionary<char, int> count = new Dictionary<char, int>();
         foreach (char c in s) {
             count[c] = 1 + (count.ContainsKey(c) ? count[c] : 0);
         }
         return new string(s.OrderByDescending(c => (count[c], c)).ToArray());
+    }
+    */
+     public string FrequencySort(string s) {
+        // IDictionary<char, int> count = new Dictionary<char, int>();
+        // foreach (char c in s) {
+        //     count[c] = 1 + (count.ContainsKey(c) ? count[c] : 0);
+        // }
+        // return new string(s.OrderByDescending(c => (count[c], c)).ToArray());
+           Dictionary<char, int> dic = new Dictionary<char, int>();
+            foreach (var c in s)
+            {
+                if (dic.ContainsKey(c))
+                {
+                    dic[c]++;
+                }
+                else
+                {
+                    dic.Add(c, 1);
+                }
+            }
+         return new string(s.OrderByDescending(c => (dic[c], c)).ToArray());
     }
 }
