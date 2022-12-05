5cbef9d93c19d95040c13e4826a8fac1d76c0983
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-12-05 21:21:24 +0200

    Time: 145 ms (28.92%), Space: 36.6 MB (55.68%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cs
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MiddleNode(ListNode head) {
+        var ret = head;
+        while (head != null && head.next != null) {
+            head = head.next.next;
+            ret = ret.next;
+        }
+        return ret;
+    }
+}
