9b20eb426424bb5a469b74a480e2c95c5cf3a8b4
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-12-05 23:55:54 +0200

    Time: 228 ms (79.62%), Space: 49.8 MB (20.62%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cs
+++ 0053-maximum-subarray/0053-maximum-subarray.cs
@@ -1,3 +1,4 @@
+/*
 // use presum method
 public class Solution {
     public int MaxSubArray(int[] nums) {
@@ -7,6 +8,23 @@ public class Solution {
             curSum += nums[i];
             max = Math.Max(max, curSum - preMinSum);
             preMinSum = Math.Min(curSum, preMinSum);
+        }
+        return max;
+    }
+}
+*/
+// dynamic programming
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        if (nums == null || nums.Length == 0) return 0;
+        int n = nums.Length;
+        // dp[i] the maximun subarray ending with nums[i]
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        int max = dp[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            max = Math.Max(max, dp[i]);
         }
         return max;
     }
