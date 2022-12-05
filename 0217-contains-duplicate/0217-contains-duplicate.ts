8a76165384de1b228796ef01830c270c3f1b112c
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-12-05 21:59:20 +0200

    Time: 309 ms (17.85%), Space: 51.3 MB (54.23%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.ts
@@ -0,0 +1,29 @@
+// solation by loop and array 
+
+function containsDuplicate(nums: number[]): boolean {
+    nums.sort((n1,n2) => n1 - n2);
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]===nums[i+1]){
+           return true
+           }
+    }
+    return false
+};
+/*
+/// best solation 
+function containsDuplicate(nums: number[]): boolean {
+    if(nums.length<2){
+       return false;
+       }
+     let resmap: number[]=[]; 
+      for(let i=0;i<nums.length;i++){
+         if(resmap.some(x => x ===nums[i])){
+             return true
+             }else{
+                 resmap.push(nums[i]);
+             }
+      }
+      return false
+};
+*/
+ 
