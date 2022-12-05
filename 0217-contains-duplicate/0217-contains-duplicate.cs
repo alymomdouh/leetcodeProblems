dabf9a5b9f9e4db239d3ec40245a0e390005c1b7
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-12-05 23:06:57 +0200

    Time: 365 ms (55.42%), Space: 47.8 MB (58.31%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cs
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -18,12 +18,24 @@ public class Solution {
     }
 }
 */
+/*
 using System.Linq;
-
 public partial class Solution
 {
     public bool ContainsDuplicate(int[] nums)
     {
         return nums.Distinct().Count() < nums.Length;
+    }
+}
+*/
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        var digits = new HashSet<int>();
+        for(int i = 0; i < nums.Length; i++){
+            if(digits.Contains(nums[i]))
+                return true; 
+            digits.Add(nums[i]); 
+        } 
+        return false;
     }
 }
