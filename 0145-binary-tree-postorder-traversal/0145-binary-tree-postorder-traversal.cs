af2e496e9820935f89fce72791e7de7fc002c6b1
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-12-14 20:29:25 +0200

    Time: 140 ms (93.57%), Space: 42.1 MB (18.01%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cs
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cs
@@ -28,17 +28,46 @@
 // }
 
 // iteration1: reverse order insert, similar to two stack.
+// public class Solution {
+//     public IList<int> PostorderTraversal(TreeNode root) {
+//         var res = new List<int>();
+//         if (root == null) return res;
+//         Stack<TreeNode> stack = new Stack<TreeNode>();
+//         stack.Push(root);
+//         while (stack.Count > 0) {
+//             root = stack.Pop();
+//             res.Insert(0, root.val);
+//             if (root.left != null) stack.Push(root.left);
+//             if (root.right != null) stack.Push(root.right);
+//         }
+//         return res;
+//     }
+// }
+
+// two stack
 public class Solution {
     public IList<int> PostorderTraversal(TreeNode root) {
         var res = new List<int>();
         if (root == null) return res;
-        Stack<TreeNode> stack = new Stack<TreeNode>();
-        stack.Push(root);
-        while (stack.Count > 0) {
-            root = stack.Pop();
-            res.Insert(0, root.val);
-            if (root.left != null) stack.Push(root.left);
-            if (root.right != null) stack.Push(root.right);
+        // Create two stacks
+        var s1 = new Stack<TreeNode>();
+        var s2 = new Stack<TreeNode>();
+        // push root to first stack
+        s1.Push(root);
+        // Run while first stack is not empty
+        while (s1.Count > 0) {
+            // Pop an item from s1 and push it to s2
+            var temp = s1.Pop();
+            s2.Push(temp);
+            // Push left and right children of removed
+            // item to s1
+            if (temp.left != null) s1.Push(temp.left);
+            if (temp.right != null) s1.Push(temp.right);
+        }
+        // record all itmes in second stack
+        while (s2.Count > 0) {
+            var node = s2.Pop();
+            res.Add(node.val);
         }
         return res;
     }
