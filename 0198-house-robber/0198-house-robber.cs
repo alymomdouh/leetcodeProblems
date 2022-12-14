31b78bfeb2c538d454575f53c066f75b76215f60
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-12-14 19:43:42 +0200

    Time: 91 ms (85.12%), Space: 38.1 MB (8.06%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int Rob(int[] nums) {
+        int prev = 0, ret = 0, curr = 0;
+        foreach (int num in nums) {
+            int rob = prev + num;
+            ret = Math.Max(ret, rob);
+            prev = Math.Max(prev, curr);
+            curr = rob;
+        }
+        return ret;
+    }
+}
