ec0d764cb5e86cc60ffe15838c633d47a1dcb29a
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-12-14 20:28:38 +0200

    Time: 157 ms (80.33%), Space: 42.1 MB (16.54%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cs
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cs
@@ -12,17 +12,34 @@
  * }
  */
 // recursion
+// public class Solution {
+//     public IList<int> PostorderTraversal(TreeNode root) {
+//         var res = new List<int>();
+//         if (root == null) return res;
+//         if (root.left != null) {
+//             res.AddRange(PostorderTraversal(root.left));
+//         }
+//         if (root.right != null) {
+//             res.AddRange(PostorderTraversal(root.right));
+//         }
+//         res.Add(root.val);
+//         return res;
+//     }
+// }
+
+// iteration1: reverse order insert, similar to two stack.
 public class Solution {
     public IList<int> PostorderTraversal(TreeNode root) {
         var res = new List<int>();
         if (root == null) return res;
-        if (root.left != null) {
-            res.AddRange(PostorderTraversal(root.left));
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.Push(root);
+        while (stack.Count > 0) {
+            root = stack.Pop();
+            res.Insert(0, root.val);
+            if (root.left != null) stack.Push(root.left);
+            if (root.right != null) stack.Push(root.right);
         }
-        if (root.right != null) {
-            res.AddRange(PostorderTraversal(root.right));
-        }
-        res.Add(root.val);
         return res;
     }
 }
