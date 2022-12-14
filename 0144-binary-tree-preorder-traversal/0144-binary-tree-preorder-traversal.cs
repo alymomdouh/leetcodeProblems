0d0b0755ea77847e1c58cb85d9f2ea229b36c38a
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-12-14 20:23:49 +0200

    Time: 152 ms (84.01%), Space: 41.9 MB (18.56%) - LeetHub

---
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+// recursion
+public class Solution {
+    public IList<int> PreorderTraversal(TreeNode root) {
+        var res = new List<int>();
+        if (root == null) return res;
+        res.Add(root.val);
+        if (root.left != null){
+            res.AddRange(PreorderTraversal(root.left));
+        }
+        if (root.right != null) {
+            res.AddRange(PreorderTraversal(root.right));
+        }
+        return res;
+    }
+}
