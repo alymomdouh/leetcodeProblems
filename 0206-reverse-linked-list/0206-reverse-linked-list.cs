02ff97110dab6b7a6a953654613477c4362c1bf0
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-12-14 20:12:07 +0200

    Time: 82 ms (96.62%), Space: 38.9 MB (14.74%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cs
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cs
@@ -9,15 +9,26 @@
  *     }
  * }
  */
+// public class Solution {
+//     public ListNode ReverseList(ListNode head) {
+//          ListNode pre = null, temp = null;
+//         while (head != null) {
+//             temp = head.next;
+//             head.next = pre;
+//             pre = head;
+//             head = temp;
+//         }
+//         return pre;
+//     }
+// }
+
+// recursion
 public class Solution {
     public ListNode ReverseList(ListNode head) {
-         ListNode pre = null, temp = null;
-        while (head != null) {
-            temp = head.next;
-            head.next = pre;
-            pre = head;
-            head = temp;
-        }
-        return pre;
+        if (head == null || head.next == null) return head;
+        ListNode newHead = ReverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
     }
 }
