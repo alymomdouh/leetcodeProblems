3b09d3db88ccb60f6f9524961c51b604fbf6f1d1
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2022-12-17 11:19:42 +0200

    Time: 84 ms (97.98%), Space: 40.3 MB (13.38%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+public static class Ext {
+    public static void PopAndApply<T>(this IList<T> list, Func<T, T, T> func)
+    {
+        T a = list[list.Count - 2], b = list[list.Count - 1];
+        list.RemoveAt(list.Count - 1);
+        list.RemoveAt(list.Count - 1);
+        list.Add(func(a, b));
+    }
+}
+
+public class Solution {
+    public int EvalRPN(string[] tokens) {
+        var stack = new List<int>();
+        foreach (string token in tokens) {
+            switch (token) {
+                case "-":
+                    stack.PopAndApply((a, b) => a - b);
+                    break;
+                case "+":
+                    stack.PopAndApply((a, b) => a + b);
+                    break;
+                case "/":
+                    stack.PopAndApply((a, b) => a / b);
+                    break;
+                case "*":
+                    stack.PopAndApply((a, b) => a * b);
+                    break;
+                default:
+                    stack.Add(int.Parse(token));
+                    break;
+            }
+        }
+        return stack[0];
+    }
+}
