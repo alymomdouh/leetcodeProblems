4f6fa03d4056a0f688629398cc957c51dd4809ba
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2023-01-01 10:11:55 +0200

    Time: 81 ms (80.66%), Space: 38.3 MB (6.93%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic; 
+public class Solution {
+    public bool WordPattern(string pattern, string str) {
+        var map = new Dictionary<string, string>();
+        var split = str.Split(' ');
+        int n = pattern.Length;
+        if (split.Length != n) {
+            return false;
+        }
+        for (int i = 0; i < n; ++i) {
+            string p = $"${pattern[i]}", s = split[i];
+            if (map.ContainsKey(p)) {
+                if (map[p] != s) {
+                    return false;
+                }
+            } else {
+                if (map.ContainsKey(s)) {
+                    return false;
+                }
+                map[p] = s;
+                map[s] = p;
+            }
+        }
+        return true;
+    }
+}
