08ea5e6a3720aa6478f6bb8f05f4b10e29d0d2c6
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2023-01-02 22:46:44 +0200

    Time: 155 ms (6.67%), Space: 36.9 MB (97.50%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cs
+++ 0520-detect-capital/0520-detect-capital.cs
@@ -1,3 +1,4 @@
+/*
 using System.Text.RegularExpressions;
 public class Solution {
 
@@ -5,5 +6,34 @@ public class Solution {
 
     public bool DetectCapitalUse(string word) {
         return Pattern.IsMatch(word);
+    }
+}
+*/
+public class Solution {
+    public bool DetectCapitalUse (string word) {
+        bool fg = false;
+        int count = 0;
+        for (int i = 0; i < word.Length; i++) {
+            char c = word[i];
+            if (i == 0) {
+                if (c >= 'A' && c <= 'Z') {
+                    fg = true;
+                    count++;
+                } else {
+                    fg = false;
+                }
+            } else {
+                if (fg) {
+                    if (c >= 'A' && c <= 'Z') {
+                        count++;
+                    }
+                } else {
+                    if (c >= 'A' && c <= 'Z') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return (count == 0 || count == 1 || count == word.Length);
     }
 }
