df392ef650655021de546d16d8f90022276761b6
Aly Momdouh Aly Sayed <33808080+alymomdouh@users.noreply.github.com>
2023-01-06 15:11:06 +0200

    Time: 329 ms (55.56%), Space: 49.9 MB (72.22%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cs
@@ -0,0 +1,46 @@
+public class Solution {
+   public int MaxIceCream(int[] costs, int coins)
+    {
+        var count = 0;
+
+        // Calculate whether O(n log n) solution would be
+        // faster than O(n) solution
+        if (costs.Length * Math.Log(costs.Length, 2) < coins)
+        {
+            Array.Sort(costs);
+            foreach (var cost in costs)
+            {
+                if (coins < cost)
+                    break;
+
+                coins -= cost;
+                count++;
+            }
+        }
+        else
+        {
+            var d = new Dictionary<int, int>();
+            foreach (var cost in costs)
+            {
+                if (d.TryGetValue(cost, out var val))
+                    d[cost] = val + 1;
+                else
+                    d[cost] = 1;
+            }
+
+            for (int icCost = 1; icCost <= coins; icCost++)
+            {
+                if (d.TryGetValue(icCost, out var numIceCreams))
+                {
+                    while (numIceCreams > 0 && coins - icCost >= 0)
+                    {
+                        count++;
+                        coins -= icCost;
+                        numIceCreams--;
+                    }
+                }
+            }
+        }
+        return count; 
+    }
+}
